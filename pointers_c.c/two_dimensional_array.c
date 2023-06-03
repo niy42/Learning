a8185da8a21fed7e1346ec10be6c2d84d4b5845c
Andrew <obanlaniyi42@gmail.com>
2023-06-03 16:41:44 +0100

    learning to program in C

---
+++ pointers_c.c/two_dimensional_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints two dimensional array of integer elements
+ * Return: Always zero main - prints two dimensional array of integer elements
+ */
+int main(void)
+{
+	int i, j;
+	int row = 3, col = 3;
+	int arr[row][col];
+
+	i = 0;
+	while (i < row)
+	{
+		j = 0;
+		for (; j < col; j++)
+		{
+			printf("Enter number of array in position arr[%d][%d] =  ", i, j); 
+			scanf("%d", &*(*(arr + i) + j));
+		}
+		++i;
+	}
+	i = 0;
+	for (; i < row; ++i)
+	{
+		printf("\n");
+		j = 0;
+		do {
+			printf("%d", *(*(arr + i) + j));
+			++j;
+		} while (j < col);
+	}
+	return (0);
+}
